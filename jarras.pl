80c8497c9239feb7952954c55eb6f99fe1921234
Guilherme Avelino <guiavenas@gmail.com>
2022-05-22 21:18:54 -0300

    adicionando letras d e f

--- jarras.pl
+++ jarras.pl
@@ -8,6 +8,16 @@ acao((J1,J2),encher1,(J1a,J2a)):-
 	J1a = C1,
 	J2a = J2.
 
+acao((J1,J2),passar12,(J1a,J2a)):-
+	capacidade((_,C2)),
+	J1 > 0,
+	J2 < C2,
+	min_list([J1, C2-J2], DIFF),
+	J2a is J2 + DIFF,
+	J2a =< C2,
+	J1a is J1 - DIFF,
+	J1a >= 0.
+
 acao((J1,J2),encher2,(J1a,J2a)):-
 	capacidade((_, C2)),
 	J2 < C2,
@@ -24,16 +34,6 @@ acao((J1,J2),esvaziar2,(J1a,J2a)):-
 	J1a = J1,
 	J2a is 0.
 
-acao((J1,J2),passar12,(J1a,J2a)):-
-	capacidade((_,C2)),
-	J1 > 0,
-	J2 < C2,
-	min_list([J1, C2-J2], DIFF),
-	J2a is J2 + DIFF,
-	J2a =< C2,
-	J1a is J1 - DIFF,
-	J1a >= 0.
-
 acao((J1,J2),passar21,(J1a,J2a)):-
 	capacidade((C1,_)),
 	J1 < C1,
@@ -44,5 +44,39 @@ acao((J1,J2),passar21,(J1a,J2a)):-
 	J2a is J2 - DIFF,
 	J2a >= 0.
 
-vizinhos(N, FronteiraN):-
-	findall(X, acao(N, _, X), FronteiraN).
+vizinhos(N, VizinhosN):-
+	findall(X, acao(N, _, X), VizinhosN).
+
+/*Letra D*/
+/*Desta forma, ficará retornando true infinitamente(ou uma grande quantidade de vezes)*/
+/*
+busca([EstadoAtual|_]):-
+	objetivo(EstadoAtual).
+
+busca([EstadoAtual| RestoDosEstados]):-
+	vizinhos(EstadoAtual, VizinhosEstadoAtual),
+	append(RestoDosEstados, VizinhosEstadoAtual, Fronteira),
+	busca(Fronteira).
+*/
+
+/*Letra F*/
+/*Evitando estados repetidos*/
+/*
+busca_interna([EstadoAtual|_], _):-
+	objetivo(EstadoAtual).
+
+busca_interna([EstadoAtual| RestoDosEstados], NosGeradosAnteriormente):-
+	\+objetivo(EstadoAtual),
+	member(EstadoAtual, NosGeradosAnteriormente),
+	busca_interna(RestoDosEstados, NosGeradosAnteriormente).
+
+busca_interna([EstadoAtual| RestoDosEstados], NosGeradosAnteriormente):-
+	\+objetivo(EstadoAtual),
+	\+member(EstadoAtual, NosGeradosAnteriormente),
+	vizinhos(EstadoAtual, VizinhosEstadoAtual),
+	append(RestoDosEstados, VizinhosEstadoAtual, Fronteira),
+	busca_interna(Fronteira, [EstadoAtual | NosGeradosAnteriormente]).
+
+busca(FronteiraInicial):-
+	busca_interna(FronteiraInicial, []).
+*/
