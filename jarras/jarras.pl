6efc0a5b14db4ae45ada2376997e222913dd35b1
Guilherme Avelino <guiavenas@gmail.com>
2022-05-25 09:11:15 -0300

    adicionando letra e

--- jarras/jarras.pl
+++ jarras/jarras.pl
@@ -3,10 +3,26 @@ objetivo((_, 2)).
 capacidade((4,3)).
 
 acao((J1,J2),encher1,(J1a,J2a)):-
-	capacidade((C1, _)),
-	J1 < C1,
-	J1a = C1,
-	J2a = J2.
+                capacidade((C1, _)),
+                J1 < C1,
+                J1a = C1,
+                J2a = J2.
+
+acao((J1,J2),encher2,(J1a,J2a)):-
+                capacidade((_, C2)),
+                J2 < C2,
+                J1a = J1,
+                J2a = C2.
+
+acao((J1,J2),esvaziar1,(J1a,J2a)):-
+                J1 > 0,
+                J1a is 0,
+                J2a = J2.
+
+acao((J1,J2),esvaziar2,(J1a,J2a)):-
+                J2 > 0,
+                J1a = J1,
+                J2a is 0.
 
 acao((J1,J2),passar12,(J1a,J2a)):-
 	capacidade((_,C2)),
@@ -18,90 +34,100 @@ acao((J1,J2),passar12,(J1a,J2a)):-
 	J1a is J1 - DIFF,
 	J1a >= 0.
 
-acao((J1,J2),encher2,(J1a,J2a)):-
-	capacidade((_, C2)),
-	J2 < C2,
-	J1a = J1,
-	J2a = C2.
-
-acao((J1,J2),esvaziar1,(J1a,J2a)):-
-	J1 > 0,
-	J1a is 0,
-	J2a = J2.
-
-acao((J1,J2),esvaziar2,(J1a,J2a)):-
-	J2 > 0,
-	J1a = J1,
-	J2a is 0.
-
 acao((J1,J2),passar21,(J1a,J2a)):-
-	capacidade((C1,_)),
-	J1 < C1,
-	J2 > 0,
-	min_list([J2, C1-J1], DIFF),
-	J1a is J1 + DIFF,
-	J1a =< C1,
-	J2a is J2 - DIFF,
-	J2a >= 0.
+                capacidade((C1,_)),
+                J1 < C1,
+                J2 > 0,
+                min_list([J2, C1-J1], DIFF),
+                J1a is J1 + DIFF,
+                J1a =< C1,
+                J2a is J2 - DIFF,
+                J2a >= 0.
 
 vizinhos(N, VizinhosN):-
-	findall(X, acao(N, _, X), VizinhosN).
+        findall(X, acao(N, _, X), VizinhosN).
 
 /*Letra D*/
+
 /*Desta forma, ficará retornando true infinitamente(ou uma grande quantidade de vezes)*/
-/*
+
+/**/
+
 busca([EstadoAtual|_]):-
-	objetivo(EstadoAtual).
+                objetivo(EstadoAtual).
+
+   
 
 busca([EstadoAtual| RestoDosEstados]):-
-	vizinhos(EstadoAtual, VizinhosEstadoAtual),
-	append(RestoDosEstados, VizinhosEstadoAtual, Fronteira),
-	busca(Fronteira).
-*/
+                vizinhos(EstadoAtual, VizinhosEstadoAtual),
+                append(RestoDosEstados, VizinhosEstadoAtual, Fronteira),
+                busca(Fronteira).
+/**/
 
+/*Letra E*/
+
+/*Salvando a configuraçao de estados na variável ListaEstados*/
+
+/**/
+busca([EstadoAtual|_], ListaEstados):-
+                objetivo(EstadoAtual),
+    			ListaEstados = [EstadoAtual].
+
+busca([EstadoAtual| RestoDosEstados], ListaEstados):-
+    			\+objetivo(EstadoAtual),
+                vizinhos(EstadoAtual, VizinhosEstadoAtual),
+                append(RestoDosEstados, VizinhosEstadoAtual, Fronteira),
+                busca(Fronteira, ListaEstadosAux),
+    			!,
+    			append([EstadoAtual], ListaEstadosAux, ListaEstados).
+/**/
 /*Letra F*/
+
 /*Evitando estados repetidos*/
-/*
+/**/
 busca_interna([EstadoAtual|_], _):-
-	objetivo(EstadoAtual).
+                objetivo(EstadoAtual).
 
 busca_interna([EstadoAtual| RestoDosEstados], NosGeradosAnteriormente):-
-	\+objetivo(EstadoAtual),
-	member(EstadoAtual, NosGeradosAnteriormente),
-	busca_interna(RestoDosEstados, NosGeradosAnteriormente).
+                \+objetivo(EstadoAtual),
+                member(EstadoAtual, NosGeradosAnteriormente),
+                busca_interna(RestoDosEstados, NosGeradosAnteriormente).
 
 busca_interna([EstadoAtual| RestoDosEstados], NosGeradosAnteriormente):-
-	\+objetivo(EstadoAtual),
-	\+member(EstadoAtual, NosGeradosAnteriormente),
-	vizinhos(EstadoAtual, VizinhosEstadoAtual),
-	append(RestoDosEstados, VizinhosEstadoAtual, Fronteira),
-	busca_interna(Fronteira, [EstadoAtual | NosGeradosAnteriormente]).
+                \+objetivo(EstadoAtual),
+                \+member(EstadoAtual, NosGeradosAnteriormente),
+                vizinhos(EstadoAtual, VizinhosEstadoAtual),
+                append(RestoDosEstados, VizinhosEstadoAtual, Fronteira),
+                busca_interna(Fronteira, [EstadoAtual | NosGeradosAnteriormente]).
 
 busca(FronteiraInicial):-
-	busca_interna(FronteiraInicial, []).
-*/
+                busca_interna(FronteiraInicial, []).
+/**/
+
+ 
 
 /*Letra G*/
+
 /* Para busca em profundidade, basta trocar a ordem dos parametros:
-Como estava: append(RestoDosEstados, VizinhosEstadoAtual, Fronteira)
-Como tem que ficar: append(VizinhosEstadoAtual, RestoDosEstados, Fronteira)
-*/
-/*
+Como estava na busca em largura: append(RestoDosEstados, VizinhosEstadoAtual, Fronteira)
+Como ficará na busca em profundidade: append(VizinhosEstadoAtual, RestoDosEstados, Fronteira)*/
+
+/**/
 busca_interna([EstadoAtual|_], _):-
-	objetivo(EstadoAtual).
+                objetivo(EstadoAtual).
 
 busca_interna([EstadoAtual| RestoDosEstados], NosGeradosAnteriormente):-
-	\+objetivo(EstadoAtual),
-	member(EstadoAtual, NosGeradosAnteriormente),
-	busca_interna(RestoDosEstados, NosGeradosAnteriormente).
+                \+objetivo(EstadoAtual),
+                member(EstadoAtual, NosGeradosAnteriormente),
+                busca_interna(RestoDosEstados, NosGeradosAnteriormente).
 
 busca_interna([EstadoAtual| RestoDosEstados], NosGeradosAnteriormente):-
-	\+objetivo(EstadoAtual),
-	\+member(EstadoAtual, NosGeradosAnteriormente),
-	vizinhos(EstadoAtual, VizinhosEstadoAtual),
-	append(VizinhosEstadoAtual, RestoDosEstados, Fronteira),
-	busca_interna(Fronteira, [EstadoAtual | NosGeradosAnteriormente]).
+                \+objetivo(EstadoAtual),
+                \+member(EstadoAtual, NosGeradosAnteriormente),
+                vizinhos(EstadoAtual, VizinhosEstadoAtual),
+                append(VizinhosEstadoAtual, RestoDosEstados, Fronteira),
+                busca_interna(Fronteira, [EstadoAtual | NosGeradosAnteriormente]).
 
 busca(FronteiraInicial):-
-	busca_interna(FronteiraInicial, []).
-*/
+                busca_interna(FronteiraInicial, []).
+/**/
